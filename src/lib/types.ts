816a1aff4e26929179f29cee0be9c600a4a2b3a1
CodeBoy124 <koen7maart@gmail.com>
2026-08-19 10:18:17 +0200

    feat: add names of 10 commandments

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -10,9 +10,15 @@ export interface SinEntry {
 	sin: string;
 }
 
+export enum CommandmentsVariant {
+	CATHOLIC,
+	PROTESTANT
+}
+
 export interface AppStorageStructure {
 	sins: SinEntry[];
 	last_confession: number; // in days since Jan. 1st 1970
+	commandments_variant?: CommandmentsVariant;
 }
 
 export enum ScreenKind {
