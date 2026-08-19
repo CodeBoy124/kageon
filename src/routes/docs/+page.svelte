60e0992f3ade7657ad5c6233cf25e4e1829518df
CodeBoy124 <koen7maart@gmail.com>
2026-08-19 09:53:10 +0200

    feat: setup basic documentation infrastructure

--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -1,1 +1,77 @@
-<h1>🚧 Work in progress 🚧</h1>
+<script lang="ts">
+	import { ScreenKind } from '$lib/types';
+
+	interface DocElement {
+		title: string;
+		image: string;
+		description: string;
+	}
+
+	type DocPage = {
+		title: string;
+		screen: ScreenKind;
+		elements: DocElement[];
+	};
+
+	type DocAll = DocPage[];
+
+	const my_docs: DocAll = [
+		{
+			title: 'Welcome',
+			screen: ScreenKind.PASSWORD,
+			elements: [
+				{
+					title: 'Password field',
+					image: 'https://dummyimage.com/300', // TODO
+					description:
+						"This website encrypts it's data using this key. The website keeps all data on your device, but even if a virus would be on there, it would be very hard for hackers to obtain your data"
+				},
+				{
+					title: 'Password field2',
+					image: 'https://dummyimage.com/300', // TODO
+					description:
+						"This website encrypts it's data using this key. The website keeps all data on your device, but even if a virus would be on there, it would be very hard for hackers to obtain your data"
+				}
+			]
+		},
+		{
+			title: 'Overview',
+			screen: ScreenKind.OVERVIEW,
+			elements: [
+				{
+					title: 'Password field',
+					image: 'https://dummyimage.com/300', // TODO
+					description:
+						"This website encrypts it's data using this key. The website keeps all data on your device, but even if a virus would be on there, it would be very hard for hackers to obtain your data"
+				},
+				{
+					title: 'Password field2',
+					image: 'https://dummyimage.com/300', // TODO
+					description:
+						"This website encrypts it's data using this key. The website keeps all data on your device, but even if a virus would be on there, it would be very hard for hackers to obtain your data"
+				}
+			]
+		}
+	];
+</script>
+
+<main>
+	<ul class="verticle">
+		{#each my_docs as doc_page (doc_page.screen)}
+			<li>
+				<h2 class="doc-page-title">"{doc_page.title}" page</h2>
+				<ul class="doc-element-container">
+					{#each doc_page.elements as doc_element (doc_element.title)}
+						<li class="doc-element">
+							<img src={doc_element.image} alt="" />
+							<div class="doc-element-desc">
+								<h3>{doc_element.title}</h3>
+								<p>{doc_element.description}</p>
+							</div>
+						</li>
+					{/each}
+				</ul>
+			</li>
+		{/each}
+	</ul>
+</main>
--- src/screens/PasswordScreen.svelte
+++ src/screens/PasswordScreen.svelte
@@ -28,7 +28,7 @@
 
 <h3>Usefull links</h3>
 <ul class="text-center">
-	<li><a href={resolve('/docs')}>🚧Beginner tour🚧</a></li>
+	<li><a href={resolve('/docs')}>⭐Beginner tour</a></li>
 	<li><a href="https://www.bible.com/bible/111/EXO.20">Exodus 20 (ten commandments)</a></li>
 	<li>
 		<a href="https://www.jesuits.org/spirituality/the-ignatian-examen/">Daily Examine</a>
